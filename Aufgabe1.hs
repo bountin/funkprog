36a5ec9751033de974f0d3e3d5ff67156d32ea49
Martin Prebio <mp@25th-floor.com>
2012-10-19 03:34:56 +0200

    Documentation++

--- Aufgabe1.hs
+++ Aufgabe1.hs
@@ -1,14 +1,17 @@
 module Main where
+	-- fac: Calculation of Faktorielle: n!
 	fac :: Integer -> Integer
 	fac n
 		| n > 0  = n * fac(n-1)
 		| otherwise = 1
 
+	-- binom: Like 49 over 6
 	binom :: (Integer, Integer) -> Integer
 	binom (n,k)
 		| n > 0 && k > 0  = div (fac n) ((fac k)  * fac (n-k))
 		| otherwise       = error "Binom error  "
 
+	-- Catalan number for example 1
 	katNumber :: Integer -> Integer
 	katNumber n
 		| n > 0 = div (binom ((2*n), n)) (n+1)
