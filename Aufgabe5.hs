aad70818855e92aa60e623503d8ecb22151f8dd7
Martin Prebio <mp@25th-floor.com>
2012-11-15 17:03:57 +0100

    Implementing assignment 5 - part II

--- Aufgabe5.hs
+++ Aufgabe5.hs
@@ -1,5 +1,7 @@
 module Aufgabe5 where
 
+import List
+
 data Tree = Null | Tree Label Tree Tree deriving (Eq,Show)
 type Label = Integer
 
@@ -27,4 +29,52 @@ f2 = \x y z -> x*y*z
 t1 = Null
 t2 = Tree 2 (Tree 3 Null Null) (Tree 5 Null Null)
 t3 = Tree 2 (Tree 3 (Tree 5 Null Null) Null) (Tree 7 Null Null)
+-}
+
+{- ************** SECOND PART WITH ERDOS ******************** -}
+{- TODO: Write a paper with a person who has written a paper with Erdos -}
+
+type ErdosNumber = Integer
+data Scientist = Sc Initial SurName  deriving (Eq,Show)
+type Initial = Char
+type SurName = String
+type Author = Scientist
+newtype Database = Db [([Author],PaperTitle)]
+type PaperTitle = String
+
+erdosNum :: Database -> Scientist -> Integer
+erdosNum db sc = (\(number, _) -> number) (erdosSearch db (0, [sc]))
+
+erdosSearch :: Database -> (Integer, [Scientist]) -> (Integer, [Scientist])
+erdosSearch db (n, scientists)
+	| scientists == [] = (-1, [])
+	| or (map isErdos scientists) = (n, scientists)
+	| otherwise = erdosSearch db (n+1, extendScientists db scientists)
+
+isErdos :: Scientist -> Bool
+isErdos (Sc 'P' "Erdos") = True
+isErdos (Sc _ _) = False
+
+extendScientists :: Database -> [Scientist] -> [Scientist]
+extendScientists db orig
+	| length (nub orig) == length new_scientists = []
+	| otherwise = new_scientists
+	where new_scientists = nub [sc | x <- orig, sc <- getCoworkers db x]
+
+scientistMerge :: Database -> [Scientist] -> [Scientist] -> [Scientist]
+scientistMerge db [] list = list
+scientistMerge db (x:xs) list = x : list
+
+getCoworkers :: Database -> Scientist -> [Scientist]
+getCoworkers db sc = [ x |
+	papers <-  (\(Db d)->d) db,
+	x <- (if sc `elem` (\(a,b)->a) papers then (\(a,b)->a) papers else [])  ]
+
+
+{- Testing papers
+db = Db [([Sc 'M' "Smith",Sc 'G' "Martin",Sc 'P' "Erdos"],"Newtonian Forms of Prime Factors"),
+         ([Sc 'P' "Erdos",Sc 'W' "Reisig"],"Stuttering in Petri Nets") ,
+         ([Sc 'M' "Smith",Sc 'X' "Chen"],"First Order Derivates in Structured Programming"),
+         ([Sc 'T' "Jablonski",Sc 'Z' "Hsueh"],"Selfstabilizing Data Structures"),
+         ([Sc 'X' "Chen",Sc 'L' "Li"],"Prime Numbers and Beyond")]
 -}
