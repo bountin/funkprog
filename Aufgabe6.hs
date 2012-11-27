9ad2c854e6a923fa2158f634db86aff70f0c44cd
Martin Prebio <mp@25th-floor.com>
2012-11-27 20:10:35 +0100

    2/3 of second part of assignment 6

--- Aufgabe6.hs
+++ Aufgabe6.hs
@@ -1,9 +1,9 @@
 module Aufgabe6 where
 
-data Nat = Z | S Nat deriving (Show)
+data Nat = Z | S Nat
 
-show :: Nat -> String
-show n = (intToRoem.(\(_,x)->x).natToInt) (n, 0)
+instance Show Nat where
+	show n = (intToRoem.(\(_,x)->x).natToInt) (n, 0)
 
 natToInt :: (Nat, Int) -> (Nat, Int)
 natToInt (Z, a) = (Z, a)
@@ -30,3 +30,86 @@ intToRoem a
 
 repeatX :: a -> Int -> [a]
 repeatX c num = foldr (:) [] (take num (repeat c))
+
+{-------------------------------------
+ --- Second part of the assignment ---
+ -------------------------------------}
+
+data RatNumbers = Rat Numerator Denominator
+type Numerator = Nat
+type Denominator = Nat
+newtype NatP = NP (Nat,Nat) deriving Show
+class Nf a where
+	t2nf :: a -> a
+
+{- Negabinary Definition and functions from Aufgabe{2,3,4} -}
+
+euclid :: Int -> Int -> Int
+euclid a 0 = a
+euclid a b = euclid b (mod a b)
+
+type NegaBinary = String
+
+dropZeroes :: NegaBinary -> NegaBinary
+dropZeroes input = if (dropped == "") then "0" else dropped
+      where dropped = realDropZeroes input
+
+realDropZeroes num
+  | num == [] = ""
+  | head num == '0' = dropZeroes (tail num)
+  | otherwise = num
+
+nbIncr :: NegaBinary -> NegaBinary
+nbIncr x = (dropZeroes . reverse . nbIncrReal . reverse) ('0' : x)
+
+nbIncrReal :: NegaBinary -> NegaBinary
+nbIncrReal ('0' : t) = '1' : t
+nbIncrReal ('1' : '1' : t) = "00" ++ t
+nbIncrReal ('1' : '0' : t) = "01" ++ nbIncrReal t
+nbIncrReal [] = "1"
+
+mkCan :: NatP -> NatP
+mkCan (NP (Z, a)) = (NP (Z, a))
+mkCan (NP (a, Z)) = (NP (a, Z))
+mkCan (NP ((S a), (S b))) = mkCan (NP (a, b))
+
+{- </COPY> -}
+
+{- Helper functions to convert the data types -}
+
+intToNb :: Int -> NegaBinary
+intToNb 0 = "0"
+intToNb n = ((\(_,s)->s).intToNbReal) (n, "0")
+
+intToNbReal :: (Int, NegaBinary) -> (Int, NegaBinary)
+intToNbReal (0, n) = (0, n)
+intToNbReal (i, n) = intToNbReal (i-1, nbIncr n)
+
+natToNb :: Nat -> NegaBinary
+natToNb a = (intToNb . (\(_,s)->s) . natToInt) (a, 0)
+
+intToNat :: Int -> Nat
+intToNat a = (\(_,s)->s) (intToNatReal (a, Z))
+
+intToNatReal :: (Int, Nat) -> (Int, Nat)
+intToNatReal (0, s) = (0, s)
+intToNatReal (a, b) = intToNatReal (a-1, (S b))
+
+{- First point of this part: Write a show function for RatNumbers -}
+
+instance Show RatNumbers where
+	show (Rat a b) = (natToNb a) ++ "/" ++ (natToNb b)
+
+{- Second point: Make RatNumbers and NatP instances of Nf ('Normalform') and implement the function -}
+
+instance Nf RatNumbers where
+	t2nf (Rat Z _) = (Rat Z (S Z))
+	t2nf (Rat ao bo) = (Rat (intToNat as) (intToNat bs))
+		where
+			a = ((\(_,s)->s) . natToInt) (ao, 0)
+			b = ((\(_,s)->s) . natToInt) (bo, 0)
+			as = div a (euclid a b)
+			bs = div b (euclid a b)
+
+instance Nf NatP where
+	t2nf (NP (a, b)) = mkCan (NP (a, b))
